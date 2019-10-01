c6282b06eca48c6bfc51567859221ab9d11ffb9a
momodupi <momodupi@gmail.com>
2019-10-01 10:48:18 -0400

    Update combination.py

--- combination.py
+++ combination.py
@@ -4,15 +4,24 @@ import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 
+from datetime import datetime
 
-boxes = 10
+import logging
+
+logfile = "{}.log".format(datetime.now().strftime("%Y%m%d_%H%M%S"))
+
+logging.basicConfig(level = logging.INFO, filename = logfile)
+
+boxes = int(input("total number of boxes: "))
 
 box = np.zeros(boxes+1)
 
 for num_box in range(1, boxes+1):
     #balls = num_box
     print("num of boxes: ", num_box)
+    logging.info("num of boxes: {}".format(num_box))
 
+    total_num = 0
     for balls in range(0, num_box + 1):
         rng = []
         rng = list(range(balls+1)) * num_box
@@ -29,14 +38,18 @@ for num_box in range(1, boxes+1):
                     flag = False
             if (flag):
                 rule_p.append(i)
+            print(".", end="")
 
-        print(rule_p)
+        #print(rule_p)
         print("    ruled: ", len(rule_p), "\n")
-        box[num_box] = len(rule_p)
-
+        #box[num_box] = len(rule_p)
+        total_num += len(rule_p)
+        logging.info("{} balls has {} ways".format(balls, len(rule_p)))
+        logging.info("all combinations: {}".format(rule_p))
+    logging.info("total number: {}\n".format(total_num))
         
-plt.figure(1)
-k = np.arange(0, boxes+1, 1)
+#plt.figure(1)
+#k = np.arange(0, boxes+1, 1)
 
 #plt.plot(k, box)
 #plt.show()
